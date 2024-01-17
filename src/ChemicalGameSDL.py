f67cd9ca1464d6f01398e0e1f495bd0275552d1c
liam824css <72741715+liam824css@users.noreply.github.com>
2024-01-17 20:57:46 +0900

    SDL2

---
+++ src/ChemicalGameSDL.py
@@ -0,0 +1,10 @@
+import sdl2
+import sys
+
+displayWidth = 1180
+displayHeight = 824
+
+max_fps = 240
+
+def SDL_SetTextInputArea(rectangle_width : int,rectangle_height : int,rectangle_x_position : int,rectangle_y_position : int):
+    
---
+++ src/graphics/pixel.py
@@ -0,0 +1,3 @@
+def HexToRGBA(Hex_COLOR):
+    
+    
